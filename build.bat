777589d981bcda9e007312b75530b9a4ab6b0c2b
Martin Barker <martin-barker@hotmail.co.uk>
2023-11-04 18:47:18 +0000

    added build file

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+if [%1] == [] GOTO exit1
+
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/barkermn01/prominence-force:%1 --push .
+GOTO exitSuccess
+
+:exit1
+echo You must supply a valid tag version E.G latest
+GOTO end
+
+:exitSuccess
+echo Packages has been successfully pushed
+GOTO end
+
+:end
